000a31797e35c5af8c61e3b68aee51534a13ce8a
iris <iris@quantoptiongroup.com>
2021-04-02 07:01:25 +0000

    Updates

---
+++ precache-manifest.b67858740f5ad31b7e9e46c08fc9d16a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b59cb520853a456941709a1d514d083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "943f6dac1d2471fab01f",
+    "url": "/static/css/2.b6642dd1.chunk.css"
+  },
+  {
+    "revision": "188c50c15ed1ad685d03",
+    "url": "/static/css/main.078676c9.chunk.css"
+  },
+  {
+    "revision": "943f6dac1d2471fab01f",
+    "url": "/static/js/2.3654cacc.chunk.js"
+  },
+  {
+    "revision": "68bd64590edaddbc2c76924e3401a365",
+    "url": "/static/js/2.3654cacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188c50c15ed1ad685d03",
+    "url": "/static/js/main.45d22d5a.chunk.js"
+  },
+  {
+    "revision": "d991981448ea8f9aa859",
+    "url": "/static/js/runtime-main.cbe038c4.js"
+  }
+]);
